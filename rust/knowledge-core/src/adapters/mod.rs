b0b19d42831e285f7fa6928720bae82d8fea7939
Rishi Patel <rpatel622@gmail.com>
2026-07-18 11:30:43 -0400

    Export FastEmbed adapters

--- rust/knowledge-core/src/adapters/mod.rs
+++ rust/knowledge-core/src/adapters/mod.rs
@@ -1,5 +1,9 @@
+#[cfg(feature = "fastembed")]
+mod fastembed;
 #[cfg(feature = "xberg")]
 mod xberg;
 
+#[cfg(feature = "fastembed")]
+pub use fastembed::{FastEmbedEmbedder, FastEmbedReranker};
 #[cfg(feature = "xberg")]
 pub use xberg::XbergDocumentExtractor;
